d36d3ec7c1c35f536338909c60813f5ab45d8ff0
TastesLikeTurkey <shawn@tastesliketurkey.com>
2018-08-10 17:20:40 -0400

    Initial code commit.

---
+++ WhatIs/WhatIs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace WhatIs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var full = false;
+			var path = string.Empty;
+
+			foreach (var arg in args)
+			{
+				if (arg.StartsWith("-"))
+				{
+					if (arg == "-full")
+					{
+						full = true;
+					}
+				}
+
+				else
+				{
+					path = arg;
+				}
+			}
+
+			Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), path));
+			var info = new DirectoryInfo(path);
+			Console.WriteLine(full ? info.FullName : info.Name);
+		}
+	}
+}
